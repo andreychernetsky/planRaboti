07ce30d26473f3d825a96b7e067c7d6e71d8bd13
andric <you@example.com>
2018-10-30 21:30:50 +0300

    silra

--- js/sstu_script.js
+++ js/sstu_script.js
@@ -12,3 +12,15 @@ jQuery(function($) {
 // 3_создать якоря (data-atribute на тегах) data-start data-end
 // 4)когда scroll дойдет до data-start, на data-header добавить класс fixed
 // 5) когда scroll дойдет до data-end data-header добавить class="scroll-end"
+
+    	let avatarElem =document.getElementById('fixd-menu');
+        let avatarSourceBottom = avatarElem.getBoundingClientRect().bottom+window.pageYOffset;
+
+        window.onscroll = function() {
+            if(avatarElem.classList.contains('fixed')&&window.pageYOffset<avatarSourceBottom){
+                avatarElem.classList.remove('fixed');
+            } else if(window.pageYOffset > avatarSourceBottom){
+               avatarElem.classList.add('fixed')
+            }
+        }
+    
