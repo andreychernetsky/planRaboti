b57aaf658a31d48d44c5d7b1b50d71bdfb95ea4c
andric <you@example.com>
2018-11-13 20:20:31 +0300

    'site'

--- js/sstu_script.js
+++ js/sstu_script.js
@@ -1,9 +1,9 @@
-jQuery(function($) {
-	$(".btn-nav").on("click",function(){
-		let target = $(this).date("target");
-		$(target).toggleClass("wrap-nav__list--open")
-	});
-});
+// jQuery(function($) {
+// 	$(".btn-nav").on("click",function(){
+// 		let target = $(this).date("target");
+// 		$(target).toggleClass("wrap-nav__list--open")
+// 	});
+// });
 
 
 
@@ -13,14 +13,49 @@ jQuery(function($) {
 // 4)когда scroll дойдет до data-start, на data-header добавить класс fixed
 // 5) когда scroll дойдет до data-end data-header добавить class="scroll-end"
 
-    	let avatarElem =document.getElementById('fixd-menu');
-        let avatarSourceBottom = avatarElem.getBoundingClientRect().bottom+window.pageYOffset;
+    	// let avatarElem =document.getElementById('fixd-menu');
+     //    let avatarSourceBottom = avatarElem.getBoundingClientRect().bottom+window.pageYOffset;
 
-        window.onscroll = function() {
-            if(avatarElem.classList.contains('fixed')&&window.pageYOffset<avatarSourceBottom){
-                avatarElem.classList.remove('fixed');
-            } else if(window.pageYOffset > avatarSourceBottom){
-               avatarElem.classList.add('fixed')
-            }
-        }
+     //    window.onscroll = function() {
+     //        if(avatarElem.classList.contains('fixed')&&window.pageYOffset<avatarSourceBottom){
+     //            avatarElem.classList.remove('fixed');
+     //        } else if(window.pageYOffset > avatarSourceBottom){
+     //           avatarElem.classList.add('fixed')
+     //        }
+     //    }
+
+       jQuery(document).ready(function ($) {
+            // Клик на элемент по которому и будет совершаться
+            // действие открытие и закрытие sidebar
+            $('.icon-menu').click(function (e) {
+                // Класс sidebar
+                var sidebar = $('.sidebar-left');
+                // Ширина sidebar
+                var sidebarWidth = $('.sidebar-left').width();
+                // Добавляем класс active
+                $('.main-header, .content').addClass('active');
+
+                // Проверяем, если в sidebar свойство left не равна ширины его то выполняються
+                // следующие действия
+                if (sidebar.css('left') != sidebarWidth) {
+                    // Добавляем ему класс active
+                    sidebar.addClass('active');
+
+                    var firstClick = true;
+
+                    // Скрипт который будет закрывать sidebar при клике на любое место
+                    $(document).bind('click.myEvent', function (e) {
+                        if (!firstClick && $(e.target).closest('.sidebar-left').length == 0) {
+                            // Удаляем класс active
+                            sidebar.removeClass('active');
+                            // Удаляем класс active
+                            $('.main-header, .content').removeClass('active');
+                            $(document).unbind('click.myEvent');
+                        }
+                        firstClick = false;
+                    });
+                }
+                e.preventDefault();
+            });
+        });
     
